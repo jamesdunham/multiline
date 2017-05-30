178812f6a0e8e52cce3d0fd542717522594d816a
James Dunham <james@jdunham.io>
2017-05-30 18:59:03 -0400

    inherit params from read_fwf, renaming for clarity

--- R/read_multiline.R
+++ R/read_multiline.R
@@ -1,45 +1,47 @@
 #' Read multiline FWF files
 #' 
-#' @param file
-#' @param lines
-#' @param col_positions
-#' @param col_types
-#' @param skip
-#' @param ...
-#' @return ...
-#' @seealso ...
+#' @inheritParams readr::read_fwf
+#' @param ... Further arguments, passed to \code{\link[readr]{read_lines()}}.
+#' @return A data.frame.
 #' @import readr
 #' @export
 #' @examples
 #' NULL
 read_multiline = function(file, lines, col_positions, col_types = NULL, skip
-  = 0) {
+  = 0, ...) {
   assert_that(is.count(lines))
   assert_that(is.scalar(skip))
   assert_that(is.list(col_positions))
   assert_that(length(col_positions) == lines)
-  ds_lines <- read_lines(file, skip)
-  split_ds <- split_lines(ds = ds_lines, lines = lines, positions =
+  line_vector <- read_lines(file, skip)
+  split_ds <- split_lines(line_vector = line_vector, lines = lines, positions =
     col_positions, types = col_types)
   join_lines(split_ds)
 }
 
-read_lines <- function(file, skip) {
+read_lines <- function(file, skip, ...) {
+  # Read lines from file
   ds = readr::datasource(file, skip = skip)
-  readr::read_lines(ds, skip = skip)
+  readr::read_lines(ds, ...)
 }
 
-split_lines <- function(ds, lines, positions, types) {
-  index = index_lines(ds, lines)
-  split_ds = lapply(seq.int(lines), read_subset, ds = ds, positions = positions,
+split_lines <- function(line_vector, lines, positions, types) {
+  index = index_lines(line_vector, lines)
+  split_ds = lapply(seq.int(lines), read_subset, line_vector = line_vector,
+    positions = positions,
     types = types, index = index)
   assert_that(is_even_split(split_ds))
   split_ds
 }
 
-read_subset <- function(i, ds, positions, types, index) {
-  r_lines = paste0(ds[index == i], collapse = "\n")
-  readr::read_fwf(r_lines, positions[[i]], types[[i]])
+read_subset <- function(i, line_vector, positions, types, index) {
+  line_subset = paste0(line_vector[index == i], collapse = "\n")
+  if (is.null(types)) {
+    types_i = NULL
+  } else {
+    types_i = types[[i]]
+  }
+  readr::read_fwf(line_subset, positions[[i]], types_i)
 }
 
 join_lines <- function(split_ds) {
